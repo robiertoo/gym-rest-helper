6f5c7a4b7babcfd1d0ac02e955e52d8e699ab219
Roberto Maciel <roberto.maciel@cohros.com.br>
2023-07-02 14:30:24 -0300

    feat: start project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{ counter }}
+  </div>
+
+  <button @click="addSeconds(5)">5s</button>
+  <button @click="addSeconds(30)">30s</button>
+  <button @click="addSeconds(60)">1m</button>
+  <button @click="addSeconds(90)">1m30s</button>
+  <button @click="addSeconds(120)">2m</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    addSeconds(seconds) {
+      this.counter = seconds;
+
+      this.startTimer();
+    },
+    startTimer() {      
+      const interval = setInterval(() => {
+        if (this.counter === 0) {
+          return clearInterval(interval);
+        }
+        
+        this.counter--;
+      }, 1000);
+    }
+  },
+  watch: {
+    counter(newCounter) {
+      if (! newCounter) {
+        console.log('The time is over!');
+
+        const alarm = new Audio(require('@/assets/alarm-beep.wav'));
+
+        alarm.play();
+
+        setTimeout(() => alarm.pause(), 3500);
+      }
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
