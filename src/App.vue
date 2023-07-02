13d5dc011301d035307bb98997a43a9cb620f9b0
Roberto Maciel <roberto.maciel@cohros.com.br>
2023-07-02 15:15:03 -0300

    feat: add tailwind and some styles

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    {{ counter }}
-  </div>
+  <div class="bg-slate-800 h-screen">
+    <div 
+      class=" bg-slate-800 text-center h-[80vh] flex items-center justify-center"
+    >
+      <span
+        class="text-slate-500 text-9xl border-4 border-slate-500 rounded-full w-56 h-56 pt-10"
+        :class="{ 'text-red-600': counter === 0 }"
+      >
+        {{ counter }}
+      </span>
+    </div>
+  
+    <div class="flex align-center justify-center">
+      <button class="button bg-red-400" @click="stopTimer">Stop</button>
+    </div>
 
-  <button @click="addSeconds(5)">5s</button>
-  <button @click="addSeconds(30)">30s</button>
-  <button @click="addSeconds(60)">1m</button>
-  <button @click="addSeconds(90)">1m30s</button>
-  <button @click="addSeconds(120)">2m</button>
+    <div class="mt-10 flex justify-center items-center space-x-3">
+      <button class="button" @click="addSeconds(5)">5s</button>
+      <button class="button" @click="addSeconds(30)">30s</button>
+      <button class="button" @click="addSeconds(60)">1m</button>
+      <button class="button" @click="addSeconds(90)">1m30s</button>
+      <button class="button" @click="addSeconds(120)">2m</button>
+    </div>  
+  </div>
 </template>
 
 <script>
@@ -16,6 +31,7 @@ export default {
   data() {
     return {
       counter: 0,
+      interval: null,
     };
   },
   methods: {
@@ -24,15 +40,21 @@ export default {
 
       this.startTimer();
     },
-    startTimer() {      
-      const interval = setInterval(() => {
+    startTimer() {
+      clearInterval(this.interval);
+      
+      this.interval = setInterval(() => {
         if (this.counter === 0) {
-          return clearInterval(interval);
+          return clearInterval(this.interval);
         }
         
         this.counter--;
       }, 1000);
-    }
+    },
+    stopTimer() {
+      clearInterval(this.interval);
+      this.counter = 0;
+    },
   },
   watch: {
     counter(newCounter) {
@@ -50,5 +72,8 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.button {
+  @apply border border-slate-700 rounded w-32 text-slate-200 text-xl p-2
+}
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue, js}'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
